ec4e1b1f5e15e6dc26bb50c5d1ce5cf0e782916e
JPG-Consulting <opensource@jpg-consulting.es>
2015-08-24 16:04:53 +0000

    Update pibootstrap.sh

--- pibootstrap.sh
+++ pibootstrap.sh
@@ -684,6 +684,10 @@ if [ \$? -ne 0 ]; then
     exit 1
 fi
 echo -e \"${USER_PASSWORD}\n${USER_PASSWORD}\" | passwd ${USER_USERNAME}
+if [ \$? -ne 0 ]; then
+    echo \"Error: Failed set password for user ${USER_USERNAME}\"
+    exit 1
+fi
 EOF
 
 #--------------------------------------------------------------------
